ad56acd210f8dd051ff1e6c460441093e95bac26
rosenclever <rosenclever@gmail.com>
2025-03-11 20:45:33 -0300

    menu com soma e conversao funcionando e udo do tryParse

--- Aula02/Program.cs
+++ Aula02/Program.cs
@@ -1,32 +1,67 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using Aula02._1bim;
+using System.Runtime.CompilerServices;
 
-Console.WriteLine("Informe a sua idade: ");
-int idade;
-idade = int.Parse(Console.ReadLine());
-Console.WriteLine("Idade:" + idade);
-
-int valor1 = 10;
-valor1 += 20; // valor1 = valor1 + 20;
-
-SomaDoisNumeros somador = new SomaDoisNumeros();
-
-Console.WriteLine("Resultado: " + somador.Somar(20, 22));
-
-bool entradaInvalida = true;
+int opcao = 0;
 do
 {
-    try
+    Console.WriteLine("Escolha a opção desejada:");
+    Console.WriteLine("(0) Sair");
+    Console.WriteLine("(1) Somar dois números");
+    Console.WriteLine("(2) Converter Metros em Milímetros");
+    Console.Write("Opção: ");
+    opcao = Convert.ToInt32(Console.ReadLine());
+    switch (opcao)
     {
-        Console.Write("Informe o valor em metros: ");
-        int metros = int.Parse(Console.ReadLine());
-        Console.WriteLine($"O valor {metros} metros corresponde a {MetrosMilimetros.ConverterMetrosEmMilimetros(metros)} milímetros");
-        entradaInvalida = false;
-    }
-    catch (Exception e)
-    {
-        Console.WriteLine("O valor informado foi inválido.\n" + e.Message);
-    }
+        case 1:
+            Somar();
+            break;
+        case 2:
+            Convertedor();
+            break;
 
-} while (entradaInvalida);
+    }
+    Console.WriteLine("Pressione uma tecla para continuar...");
+    Console.ReadLine();
+    Console.Clear();
+} while(opcao != 0);
+
+void Somar()
+{
+    Console.Write("Informe o primeiro numero: ");
+    var valor1 = Console.ReadLine();
+    Console.Write("Informe o segundo numero: ");
+    var valor2 = Console.ReadLine();
+    if (int.TryParse(valor1, out var saidaValor1) && int.TryParse(valor2, out var saidaValor2))
+    {
+        SomaDoisNumeros somador = new SomaDoisNumeros();
+        Console.WriteLine("Resultado: " + somador.Somar(saidaValor1, saidaValor2));
+    }
+    else
+    {
+        Console.WriteLine($"Os valores informados devem ser números inteiros. Valor1: {valor1} / Valor2 {valor2}");
+    }
+    
+}
+
+void Convertedor()
+{
+    bool entradaInvalida = true;
+    do
+    {
+        try
+        {
+            Console.Write("Informe o valor em metros: ");
+            var metros = int.Parse(Console.ReadLine());
+            Console.WriteLine($"O valor {metros} metros corresponde a {MetrosMilimetros.ConverterMetrosEmMilimetros(metros)} milímetros");
+            entradaInvalida = false;
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("O valor informado foi inválido.\n" + e.Message);
+        }
+
+    } while (entradaInvalida);
+}
+
 
