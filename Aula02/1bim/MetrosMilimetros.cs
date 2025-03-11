800c5d182b3f70d84dde1fb09625232069854980
rosenclever <rosenclever@gmail.com>
2025-03-11 19:16:31 -0300

    try e loop e metrosmilimetros

---
+++ Aula02/1bim/MetrosMilimetros.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula02._1bim
+{
+    public class MetrosMilimetros
+    {
+        public static int ConverterMetrosEmMilimetros(int metros)
+        {
+            return metros * 1000;
+        }
+    }
+}
--- Aula02/Program.cs
+++ Aula02/Program.cs
@@ -9,7 +9,24 @@ Console.WriteLine("Idade:" + idade);
 int valor1 = 10;
 valor1 += 20; // valor1 = valor1 + 20;
 
-SomaDoisNumeros somador;
-somador = new SomaDoisNumeros();
+SomaDoisNumeros somador = new SomaDoisNumeros();
 
 Console.WriteLine("Resultado: " + somador.Somar(20, 22));
+
+bool entradaInvalida = true;
+do
+{
+    try
+    {
+        Console.Write("Informe o valor em metros: ");
+        int metros = int.Parse(Console.ReadLine());
+        Console.WriteLine($"O valor {metros} metros corresponde a {MetrosMilimetros.ConverterMetrosEmMilimetros(metros)} milímetros");
+        entradaInvalida = false;
+    }
+    catch (Exception e)
+    {
+        Console.WriteLine("O valor informado foi inválido.\n" + e.Message);
+    }
+
+} while (entradaInvalida);
+
